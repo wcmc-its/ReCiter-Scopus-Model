ef9b2a1d65989e8008db040b763269955cc4332c
jl987-Jie <jl987@cornell.edu>
2017-10-14 14:22:46 -0700

    Remove getters/setters

--- src/reciter/model/scopus/Affiliation.java
+++ src/reciter/model/scopus/Affiliation.java
@@ -22,46 +22,4 @@ public class Affiliation {
 	private String affilname; // <affilname> XML tag.
 	private String affiliationCity; // <affiliation-city> XML tag.
 	private String affiliationCountry; // <affiliation-country> XML tag.
-
-	public Affiliation() {}
-	
-	public Affiliation(int afid, String affilname, String affiliationCity, String affiliationCountry) {
-		this.afid = afid;
-		this.affilname = affilname;
-		this.affiliationCity = affiliationCity;
-		this.affiliationCountry = affiliationCountry;
-	}
-
-	public int getAfid() {
-		return afid;
-	}
-
-	public void setAfid(int afid) {
-		this.afid = afid;
-	}
-
-	public String getAffilname() {
-		return affilname;
-	}
-
-	public void setAffilname(String affilname) {
-		this.affilname = affilname;
-	}
-
-	public String getAffiliationCity() {
-		return affiliationCity;
-	}
-
-	public void setAffiliationCity(String affiliationCity) {
-		this.affiliationCity = affiliationCity;
-	}
-
-	public String getAffiliationCountry() {
-		return affiliationCountry;
-	}
-
-	public void setAffiliationCountry(String affiliationCountry) {
-		this.affiliationCountry = affiliationCountry;
-	}
-	
 }
--- src/reciter/model/scopus/ScopusArticle.java
+++ src/reciter/model/scopus/ScopusArticle.java
@@ -31,37 +31,4 @@ public class ScopusArticle {
 	private long pubmedId;
 	private List<Affiliation> affiliations;
 	private List<Author> authors;
-
-	public ScopusArticle() {
-	}
-
-	public ScopusArticle(long pubmedId, List<Affiliation> affiliations, List<Author> authors) {
-		this.pubmedId = pubmedId;
-		this.affiliations = affiliations;
-		this.authors = authors;
-	}
-
-	public long getPubmedId() {
-		return pubmedId;
-	}
-
-	public void setPubmedId(long pubmedId) {
-		this.pubmedId = pubmedId;
-	}
-
-	public List<Affiliation> getAffiliations() {
-		return affiliations;
-	}
-
-	public void setAffiliations(List<Affiliation> affiliations) {
-		this.affiliations = affiliations;
-	}
-
-	public List<Author> getAuthors() {
-		return authors;
-	}
-
-	public void setAuthors(List<Author> authors) {
-		this.authors = authors;
-	}
 }
